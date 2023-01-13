f320dd510216b2dfc9eb5d5008b502e0cca157f5
CHUCHUDACHUCHU <yoon7548@gmail.com>
2023-01-13 10:10:52 +0900

    real last

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+    host: 'express-database.cnw5mtlwmjco.ap-northeast-2.rds.amazonaws.com',
+    user: 'root',
+    password: 'daew09070515',
+    database: 'myapp',
+    port: 3306,
 });
 
 exports.pool = pool;
